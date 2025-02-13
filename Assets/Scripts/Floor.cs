1c5d19f583142afb6cfc6605d6bb23a31e6c04ca
Soniii-05 <dicesaresonia2005@gmail.com>
2025-02-13 17:01:58 +0100

    floor

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    Vector2 startPos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= -5.7f)
+        {
+            transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y);
+        } else 
+          {
+            transform.position = startPos;
+          }
+        
+    }
+}
